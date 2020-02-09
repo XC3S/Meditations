c540dc9dd87c27b6de3d9d93c6fb7500863c71ba
XC3S <7452070+XC3S@users.noreply.github.com>
2020-02-09 16:57:17 +0000

    fixed lambda syntax

--- amplify/backend/function/meditationsSendMail/src/index.js
+++ amplify/backend/function/meditationsSendMail/src/index.js
@@ -1,7 +1,23 @@
+var AWS = require('aws-sdk');
+//import { CognitoIdentityServiceProvider } from 'aws-sdk';
 
-
-exports.handler = async (event) => {
-    // TODO implement
+exports.handler = async function(event) {
+    const cognitoIdentityService = new AWS.CognitoIdentityServiceProvider();
+    
+    const params = {
+        UserPoolId: 'eu-central-1_DqGXBQm3S'//process.env.userPoolId
+    }
+    
+    try {
+        const data = await cognitoIdentityService.listUsers(params).promise();
+        console.log("data: ",data);
+    } 
+    catch (error) {
+        console.log('Error', error)
+    }
+    
+    console.log("test output 123");
+    
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
